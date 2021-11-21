13523acff688ebc3455b17c5c8a239e1d040f566
DzmitryYesis <yesis1988@mail.ru>
2021-11-21 18:49:02 +0300

    lessons 21. Add users in dialogs and do verstka

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,30 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Brat
+                </div>
+                <div className={s.dialog}>
+                    Kisa
+                </div>
+                <div className={s.dialog}>
+                    Vlad
+                </div>
+                <div className={s.dialog}>
+                    Starik
+                </div>
+                <div className={s.dialog}>
+                    Some man
+                </div>
+            </div>
+            <div className={s.messeges}>
+                <div className={s.messege}>Hi</div>
+                <div className={s.messege}>What is your name?</div>
+                <div className={s.messege}>Fack you, Spilberg!</div>
+
+            </div>
         </div>
     )
 }
