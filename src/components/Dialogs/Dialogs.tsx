677c6b40777b62485057dafd778e4c74c8898f35
DzmitryYesis <yesis1988@mail.ru>
2021-11-21 21:38:13 +0300

    lessons 22. Add user in dialog and add switching between user's dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from 'react-router-dom';
+
+const users = [
+    {user: 'Brat', path: '/dialogs/1'},
+    {user: 'Kisa', path: '/dialogs/2'},
+    {user: 'Vlad', path: '/dialogs/3'},
+    {user: 'Starik', path: '/dialogs/4'},
+    {user: 'Nastya', path: '/dialogs/5'},
+    {user: 'Igor', path: '/dialogs/6'},
+    {user: 'Sveta', path: '/dialogs/7'}
+]
 
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog}>
-                    Brat
-                </div>
-                <div className={s.dialog}>
-                    Kisa
-                </div>
-                <div className={s.dialog}>
-                    Vlad
-                </div>
-                <div className={s.dialog}>
-                    Starik
-                </div>
-                <div className={s.dialog}>
-                    Some man
-                </div>
+                {users.map((u) => {
+                    return (
+                        <div className={s.dialog}>
+                            <NavLink to={u.path}>{u.user}</NavLink>
+                        </div>
+                    )
+                })}
+
+                {/*<div className={s.dialog}>*/}
+                {/*    Brat*/}
+                {/*</div>*/}
+                {/*<div className={s.dialog}>*/}
+                {/*    Kisa*/}
+                {/*</div>*/}
+                {/*<div className={s.dialog}>*/}
+                {/*    Vlad*/}
+                {/*</div>*/}
+                {/*<div className={s.dialog}>*/}
+                {/*    Starik*/}
+                {/*</div>*/}
+                {/*<div className={s.dialog}>*/}
+                {/*    Some man*/}
+                {/*</div>*/}
             </div>
             <div className={s.messeges}>
                 <div className={s.messege}>Hi</div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,41 +2,43 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import s from './Navbar.module.css';
 
-const arrNavBar=[
-    {path:'/profile', title:'My Profile'},
-    {path:'/dialogs', title:'My Messeges'},
-    {path:'/news', title:'News'},
-    {path:'/music', title:'My Music'},
-    {path:'/settings', title:'Settings'}
+const arrNavBar = [
+    {path: '/profile', title: 'My Profile'},
+    {path: '/dialogs', title: 'My Messeges'},
+    {path: '/news', title: 'News'},
+    {path: '/music', title: 'My Music'},
+    {path: '/settings', title: 'Settings'}
 ]
 
 export const Navbar = () => {
     return (
         <div className={s.nav}>
-            {arrNavBar.map((el_navbar)=>{
-                return(
-                    <div className={s.item}>
-                        <NavLink to={el_navbar.path} className={({isActive})=>isActive? s.active:''}>{el_navbar.title}</NavLink>
-                    </div>
-                )
-            })}
+            {arrNavBar.map((el_navbar) => {
+                    return (
+                        <div className={s.item}>
+                            <NavLink to={el_navbar.path} className={({isActive}) => isActive ? s.active : ''}>{el_navbar.title}</NavLink>
+                        </div>
+                    )
+                }
+            )
+            }
 
 
-        {/*    <div className={s.item}>*/}
-        {/*        <NavLink to={'/profile'} className={({isActive}) => isActive ? s.active : ''}>Profile</NavLink>*/}
-        {/*    </div>*/}
-        {/*    <div className={s.item}>*/}
-        {/*        <NavLink to={'/dialogs'} className={({isActive}) => isActive ? s.active : ''}>Messages</NavLink>*/}
-        {/*    </div>*/}
-        {/*    <div className={s.item}>*/}
-        {/*        <NavLink to={'/news'} className={({isActive}) => isActive ? s.active : ''}>News</NavLink>*/}
-        {/*    </div>*/}
-        {/*    <div className={s.item}>*/}
-        {/*        <NavLink to={'/music'} className={({isActive}) => isActive ? s.active : ''}>Music</NavLink>*/}
-        {/*    </div>*/}
-        {/*    <div className={s.item}>*/}
-        {/*        <NavLink to={'/settings'} className={({isActive}) => isActive ? s.active : ''}>Settings</NavLink>*/}
-        {/*    </div>*/}
+            {/*    <div className={s.item}>*/}
+            {/*        <NavLink to={'/profile'} className={({isActive}) => isActive ? s.active : ''}>Profile</NavLink>*/}
+            {/*    </div>*/}
+            {/*    <div className={s.item}>*/}
+            {/*        <NavLink to={'/dialogs'} className={({isActive}) => isActive ? s.active : ''}>Messages</NavLink>*/}
+            {/*    </div>*/}
+            {/*    <div className={s.item}>*/}
+            {/*        <NavLink to={'/news'} className={({isActive}) => isActive ? s.active : ''}>News</NavLink>*/}
+            {/*    </div>*/}
+            {/*    <div className={s.item}>*/}
+            {/*        <NavLink to={'/music'} className={({isActive}) => isActive ? s.active : ''}>Music</NavLink>*/}
+            {/*    </div>*/}
+            {/*    <div className={s.item}>*/}
+            {/*        <NavLink to={'/settings'} className={({isActive}) => isActive ? s.active : ''}>Settings</NavLink>*/}
+            {/*    </div>*/}
         </div>
     )
 
