0885338be697ee3b7bcb0eab68c757526f6105a9
DzmitryYesis <yesis1988@mail.ru>
2022-04-05 16:20:57 +0300

    installed eslint and prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
+
 import './App.css';
-import {Route, Routes} from 'react-router-dom';
-import {Header} from './components/Header/Header';
-import {Navbar} from './components/Navbar/Navbar';
-import {Profile} from './components/Profile/Profile';
-import {News} from './components/News/News';
-import {Music} from './components/Music/Music';
-import {Settings} from './components/Settings/Settings';
-import {DialogsConteiner} from './components/Dialogs/DialogsConteiner';
+import { Route, Routes } from 'react-router-dom';
 
+import { DialogsConteiner } from './components/Dialogs/DialogsConteiner';
+import { Header } from './components/Header/Header';
+import { Music } from './components/Music/Music';
+import { Navbar } from './components/Navbar/Navbar';
+import { News } from './components/News/News';
+import { Profile } from './components/Profile/Profile';
+import { Settings } from './components/Settings/Settings';
 
-const App = () => {
-
-    return (
-        <div className={'app-wrapper'}>
-            <Header/>
-            <Navbar/>
-            <div className={'app-wrapper-content'}>
-                <Routes>
-                    <Route path={'/profile'} element={<Profile/>}/>
-                    <Route path={'/dialogs/*'} element={<DialogsConteiner/>}/>
-                    <Route path={'/news'} element={<News/>}/>
-                    <Route path={'/music'} element={<Music/>}/>
-                    <Route path={'/settings/*'} element={<Settings/>}/>
-                </Routes>
-            </div>
-        </div>
-    );
-};
-
+const App = (): ReactElement => (
+  <div className="app-wrapper">
+    <Header />
+    <Navbar />
+    <div className="app-wrapper-content">
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+        <Route path={'/dialogs/*'} element={<DialogsConteiner />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/music" element={<Music />} />
+        <Route path={'/settings/*'} element={<Settings />} />
+      </Routes>
+    </div>
+  </div>
+);
 
 export default App;
