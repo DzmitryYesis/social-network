0072098134abe81b5e4d20b7ad9d43bf9dc2e200
DzmitryYesis <yesis1988@mail.ru>
2022-04-14 16:40:22 +0300

    refactor: add eslint and prettier in Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,31 +1,39 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React, { ReactElement } from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+import { Friends } from './Friends/Friends';
 import s from './Navbar.module.css';
-import {Friends} from './Friends/Friends';
 
-
-export const Navbar = () => {
-    return (
-        <div className={s.nav}>
-            <div className={s.item}>
-                <NavLink to={'/profile'} className={({isActive}) => isActive ? s.active : ''}>Profile</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={'/dialogs'} className={({isActive}) => isActive ? s.active : ''}>Messages</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={'/news'} className={({isActive}) => isActive ? s.active : ''}>News</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={'/music'} className={({isActive}) => isActive ? s.active : ''}>Music</NavLink>
-            </div>
-            <div className={s.item}>
-                <NavLink to={'/settings'} className={({isActive}) => isActive ? s.active : ''}>Settings</NavLink>
-            </div>
-            <div>
-                <Friends/>
-            </div>
-        </div>
-    )
-
-}
+export const Navbar = (): ReactElement => (
+  <div className={s.nav}>
+    <div className={s.item}>
+      <NavLink to="/profile" className={({ isActive }) => (isActive ? s.active : '')}>
+        Profile
+      </NavLink>
+    </div>
+    <div className={s.item}>
+      <NavLink to="/dialogs" className={({ isActive }) => (isActive ? s.active : '')}>
+        Messages
+      </NavLink>
+    </div>
+    <div className={s.item}>
+      <NavLink to="/news" className={({ isActive }) => (isActive ? s.active : '')}>
+        News
+      </NavLink>
+    </div>
+    <div className={s.item}>
+      <NavLink to="/music" className={({ isActive }) => (isActive ? s.active : '')}>
+        Music
+      </NavLink>
+    </div>
+    <div className={s.item}>
+      <NavLink to="/settings" className={({ isActive }) => (isActive ? s.active : '')}>
+        Settings
+      </NavLink>
+    </div>
+    <div>
+      <Friends />
+    </div>
+  </div>
+);
