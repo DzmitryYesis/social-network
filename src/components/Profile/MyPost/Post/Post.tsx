8ebed21bae1ed9cf4a61f4010d63b5c450868a46
DzmitryYesis <yesis1988@mail.ru>
2022-04-14 17:07:17 +0300

    refactor: add eslint and prettier in Post.tsx

--- src/components/Profile/MyPost/Post/Post.tsx
+++ src/components/Profile/MyPost/Post/Post.tsx
@@ -1,23 +1,21 @@
+import React, { ReactElement } from 'react';
+
 import s from './Post.module.css';
-import React from 'react';
 
 export type PostPropsType = {
-    id: number
-    message: string
-    numberLike: number
-}
+  message: string;
+  numberLike: number;
+};
 
-const Post = ({message, numberLike, ...props}:PostPropsType) => {
-    return (
-        <div className={s.item}>
-            <img
-                src={'https://www.meme-arsenal.com/memes/58a847c6512c43f49c91cbebb847e27c.jpg'}/>
-            {message}
-            <div>
-                <span>{numberLike} likes</span>
-            </div>
-        </div>
-    )
-}
-
-export default Post
+export const Post = ({ message, numberLike }: PostPropsType): ReactElement => (
+  <div className={s.item}>
+    <img
+      src="https://www.meme-arsenal.com/memes/58a847c6512c43f49c91cbebb847e27c.jpg"
+      alt="post logo"
+    />
+    {message}
+    <div>
+      <span>{numberLike} likes</span>
+    </div>
+  </div>
+);
