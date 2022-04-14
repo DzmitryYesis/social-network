66e6871fc99ccec0d536a647c34f4d81b26e677c
DzmitryYesis <yesis1988@mail.ru>
2022-04-14 16:04:09 +0300

    refactor: apply eslint and prettier in Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
+
 import s from './Header.module.css';
 
-export const Header = () => {
-    return (
-        <header className={s.header}>
-            <img src={'https://1757140519.rsc.cdn77.org/static/v3/img/products/logo.png'}/>
-        </header>
-    )
-
-}
+export const Header = (): ReactElement => (
+  <header className={s.header}>
+    <img
+      src="https://1757140519.rsc.cdn77.org/static/v3/img/products/logo.png"
+      alt="social-network logo"
+    />
+  </header>
+);
