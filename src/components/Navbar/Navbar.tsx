a3a4d3b9d1f4807be69eeda0205401b99836e55d
DzmitryYesis <yesis1988@mail.ru>
2022-04-14 16:49:22 +0300

    fix: change structure of the NavLink in Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,30 +5,35 @@ import { NavLink } from 'react-router-dom';
 import { Friends } from './Friends';
 import s from './Navbar.module.css';
 
+import { PATH } from 'enum';
+
 export const Navbar = (): ReactElement => (
   <div className={s.nav}>
     <div className={s.item}>
-      <NavLink to="/profile" className={({ isActive }) => (isActive ? s.active : '')}>
+      <NavLink to={PATH.PROFILE} className={({ isActive }) => (isActive ? s.active : '')}>
         Profile
       </NavLink>
     </div>
     <div className={s.item}>
-      <NavLink to="/dialogs" className={({ isActive }) => (isActive ? s.active : '')}>
+      <NavLink to={PATH.DIALOGS} className={({ isActive }) => (isActive ? s.active : '')}>
         Messages
       </NavLink>
     </div>
     <div className={s.item}>
-      <NavLink to="/news" className={({ isActive }) => (isActive ? s.active : '')}>
+      <NavLink to={PATH.NEWS} className={({ isActive }) => (isActive ? s.active : '')}>
         News
       </NavLink>
     </div>
     <div className={s.item}>
-      <NavLink to="/music" className={({ isActive }) => (isActive ? s.active : '')}>
+      <NavLink to={PATH.MUSIC} className={({ isActive }) => (isActive ? s.active : '')}>
         Music
       </NavLink>
     </div>
     <div className={s.item}>
-      <NavLink to="/settings" className={({ isActive }) => (isActive ? s.active : '')}>
+      <NavLink
+        to={PATH.SETTINGS}
+        className={({ isActive }) => (isActive ? s.active : '')}
+      >
         Settings
       </NavLink>
     </div>
