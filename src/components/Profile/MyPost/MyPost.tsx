40b617acccbe16da1496925976ad3dd8ac5ec1e3
DzmitryYesis <yesis1988@mail.ru>
2022-04-18 21:30:53 +0300

    refactor: add eslint and prettier in MyPost.tsx

--- src/components/Profile/MyPost/MyPost.tsx
+++ src/components/Profile/MyPost/MyPost.tsx
@@ -1,42 +1,44 @@
+import React, { ChangeEvent, ReactElement } from 'react';
+
 import s from './MyPost.module.css';
-import React, {ChangeEvent} from 'react';
-import Post from './Post/Post';
-import {ProfilePagePropsType} from '../../../store/reducers/profileReducer';
+
+import { Post } from 'components/Profile/MyPost/Post/Post';
+import { ProfilePagePropsType } from 'types';
 
 type MyPostPropsType = {
-    addPost: () => void
-    changePostHandler: (text: string) => void
-    profilePage: ProfilePagePropsType
-}
+  addPost: () => void;
+  changePostHandler: (text: string) => void;
+  profilePage: ProfilePagePropsType;
+};
 
-const MyPost = (props: MyPostPropsType) => {
+export const MyPost = ({
+  addPost,
+  changePostHandler,
+  profilePage,
+}: MyPostPropsType): ReactElement => {
+  const addPostHandle = (): void => {
+    addPost();
+  };
+  const changePostHandle = (e: ChangeEvent<HTMLTextAreaElement>): void => {
+    changePostHandler(e.currentTarget.value);
+  };
 
-
-    const addPost = () => {
-        props.addPost()
-    }
-    const changePostHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        props.changePostHandler(e.currentTarget.value)
-    }
-
-    return (
-        <div className={s.postBlock}>
-            <h3>My posts</h3>
-            <div>
-                <textarea onChange={changePostHandler} value={props.profilePage.newPost}></textarea>
-            </div>
-            <div>
-                <button onClick={addPost}>Add post</button>
-            </div>
-            <div className={s.posts}>
-                {
-                    props.profilePage.postData.map(v => <Post key={v.id} message={v.message} numberLike={v.numberLike}
-                                                              id={v.id}/>)
-                }
-            </div>
-        </div>
-    )
-
-}
-
-export default MyPost
+  return (
+    <div className={s.postBlock}>
+      <h3>My posts</h3>
+      <div>
+        <textarea onChange={changePostHandle} value={profilePage.newPost} />
+      </div>
+      <div>
+        <button onClick={addPostHandle} type="button">
+          Add post
+        </button>
+      </div>
+      <div className={s.posts}>
+        {profilePage.postsData.map(v => (
+          <Post key={v.id} post={v.post} numberLike={v.numberLike} />
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/components/Profile/MyPost/Post/Post.tsx
+++ src/components/Profile/MyPost/Post/Post.tsx
@@ -3,17 +3,17 @@ import React, { ReactElement } from 'react';
 import s from './Post.module.css';
 
 export type PostPropsType = {
-  message: string;
+  post: string;
   numberLike: number;
 };
 
-export const Post = ({ message, numberLike }: PostPropsType): ReactElement => (
+export const Post = ({ post, numberLike }: PostPropsType): ReactElement => (
   <div className={s.item}>
     <img
       src="https://www.meme-arsenal.com/memes/58a847c6512c43f49c91cbebb847e27c.jpg"
       alt="post logo"
     />
-    {message}
+    {post}
     <div>
       <span>{numberLike} likes</span>
     </div>
