387490d9079042941c3cb138c9fcfabf8312880d
DzmitryYesis <yesis1988@mail.ru>
2022-04-19 12:49:32 +0300

    refactor: add eslint and prettier in ProfileInfo.tsx

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import s from './ProfileInfo.module.css'
+import React, { ReactElement } from 'react';
 
-export const ProfileInfo = () => {
-    return (
-        <div>
-            <img
-                src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeAnH9q1eO2cMNhym5S0zm0k3i4wvJ3tjLVg&usqp=CAU'}/>
-            <div className={s.discriptionBlock}>
-                ava+discription
-            </div>
-        </div>
-    )
-}
+import s from './ProfileInfo.module.css';
+
+export const ProfileInfo = (): ReactElement => (
+  <div>
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeAnH9q1eO2cMNhym5S0zm0k3i4wvJ3tjLVg&usqp=CAU"
+      alt="profile-logo"
+    />
+    <div className={s.discriptionBlock}>ava+description</div>
+  </div>
+);
