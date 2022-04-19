d229ee6596cc7854a51d03b8f6e0432dfe185284
DzmitryYesis <yesis1988@mail.ru>
2022-04-19 15:11:00 +0300

    fix: start project after added eslint and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -4,27 +4,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import { App } from 'App';
+import { reportWebVitals } from 'reportWebVitals';
 import { store } from 'store';
 
-const renderTree = (): void => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root'),
-  );
-};
-
-renderTree();
-
-store.subscribe(renderTree);
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
