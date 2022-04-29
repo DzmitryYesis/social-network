b8e17ee2c87943a18240922a69886a7a58d43267
DzmitryYesis <yesis1988@mail.ru>
2022-04-29 11:54:59 +0300

    feat: add some style for Users page

--- src/components/Users/User/User.tsx
+++ src/components/Users/User/User.tsx
@@ -1,6 +1,37 @@
 import { ReactElement } from 'react';
 
+import styled from 'styled-components';
+
 import { UsersPagePropsType } from 'types';
+
+type DivPropsType = {
+  direction: string;
+  justifyContent?: string;
+  alignItems?: string;
+  width?: string;
+};
+
+const LogoImg = styled.img`
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+`;
+
+const Div = styled.div<DivPropsType>`
+  display: flex;
+  flex-direction: ${({ direction }) => direction};
+  justify-content: ${({ justifyContent }) => justifyContent};
+  align-items: ${({ alignItems }) => alignItems};
+  width: ${({ width }) => width};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 600px;
+  border: 1px solid black;
+`;
 
 export const User = ({
   id,
@@ -10,10 +41,10 @@ export const User = ({
   status,
   location,
 }: UsersPagePropsType): ReactElement => (
-  <div key={id}>
-    <div>
+  <Wrapper key={id}>
+    <Div direction="column" alignItems="centre">
       <div>
-        <img src={logo} alt="user-logo" />
+        <LogoImg src={logo} alt="user-logo" />
       </div>
       <div>
         {followed ? (
@@ -22,16 +53,16 @@ export const User = ({
           <button type="button">FOLLOWED</button>
         )}
       </div>
-    </div>
-    <div>
-      <div>
+    </Div>
+    <Div direction="row" justifyContent="space-between" width="450px">
+      <Div direction="column" alignItems="flex-start" justifyContent="space-around">
         <div>{fullName}</div>
         <div>{status}</div>
-      </div>
-      <div>
+      </Div>
+      <Div direction="column" alignItems="flex-end" justifyContent="space-around">
         <div>{location.city}</div>
         <div>{location.country}</div>
-      </div>
-    </div>
-  </div>
+      </Div>
+    </Div>
+  </Wrapper>
 );
