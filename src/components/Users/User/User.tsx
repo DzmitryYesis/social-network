e7091bd9c954afda2b45240085bed8225a566538
DzmitryYesis <yesis1988@mail.ru>
2022-04-29 12:25:46 +0300

    refactor: add import styles in User.tsx. Fix bugs

--- src/components/Users/User/User.tsx
+++ src/components/Users/User/User.tsx
@@ -1,37 +1,7 @@
 import { ReactElement } from 'react';
 
-import styled from 'styled-components';
-
+import { Div, LogoImg, UserWrapper } from 'style';
 import { UsersPagePropsType } from 'types';
-
-type DivPropsType = {
-  direction: string;
-  justifyContent?: string;
-  alignItems?: string;
-  width?: string;
-};
-
-const LogoImg = styled.img`
-  height: 100px;
-  width: 100px;
-  border-radius: 50px;
-`;
-
-const Div = styled.div<DivPropsType>`
-  display: flex;
-  flex-direction: ${({ direction }) => direction};
-  justify-content: ${({ justifyContent }) => justifyContent};
-  align-items: ${({ alignItems }) => alignItems};
-  width: ${({ width }) => width};
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  width: 600px;
-  border: 1px solid black;
-`;
 
 export const User = ({
   id,
@@ -41,8 +11,8 @@ export const User = ({
   status,
   location,
 }: UsersPagePropsType): ReactElement => (
-  <Wrapper key={id}>
-    <Div direction="column" alignItems="centre">
+  <UserWrapper key={id}>
+    <Div direction="column" alignItems="flex-start">
       <div>
         <LogoImg src={logo} alt="user-logo" />
       </div>
@@ -64,5 +34,5 @@ export const User = ({
         <div>{location.country}</div>
       </Div>
     </Div>
-  </Wrapper>
+  </UserWrapper>
 );
