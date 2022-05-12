7424c5195e1e6329b4ad0778569bbc4ec182bbe1
DzmitryYesis <yesis1988@mail.ru>
2022-05-12 16:19:09 +0300

    feat: create action for new users state

--- src/store/actions/usersActions.ts
+++ src/store/actions/usersActions.ts
@@ -1,10 +1,35 @@
-import { UsersPagePropsType } from 'types';
+import { UsersBLLType } from 'store/reducers/usersReducer';
+// import { UsersPagePropsType } from 'types';
 
 export const FOLLOWED = 'USERS/FOLLOWED';
 export const UNFOLLOWED = 'USERS/UNFOLLOWED';
 export const SET_STATE = 'USERS/SET-STATE';
 
-export const followedAC = (userID: string) =>
+// export const followedAC = (userID: string) =>
+//   ({
+//     type: FOLLOWED,
+//     payload: {
+//       userID,
+//     },
+//   } as const);
+//
+// export const unFollowedAC = (userID: string) =>
+//   ({
+//     type: UNFOLLOWED,
+//     payload: {
+//       userID,
+//     },
+//   } as const);
+//
+// export const setUsersAC = (state: UsersPagePropsType[]) =>
+//   ({
+//     type: SET_STATE,
+//     payload: {
+//       state,
+//     },
+//   } as const);
+
+export const followedAC = (userID: number) =>
   ({
     type: FOLLOWED,
     payload: {
@@ -12,7 +37,7 @@ export const followedAC = (userID: string) =>
     },
   } as const);
 
-export const unFollowedAC = (userID: string) =>
+export const unFollowedAC = (userID: number) =>
   ({
     type: UNFOLLOWED,
     payload: {
@@ -20,7 +45,7 @@ export const unFollowedAC = (userID: string) =>
     },
   } as const);
 
-export const setUsersAC = (state: UsersPagePropsType[]) =>
+export const setUsersAC = (state: UsersBLLType[]) =>
   ({
     type: SET_STATE,
     payload: {
